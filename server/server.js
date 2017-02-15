346b436052382ce56bc40e470bd3e5d9f4cd722d
David Chang <dchang103@gmail.com>
2017-02-14 19:47:55 -0800

    basic express server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes.js');
+const app = express();
+const port = 3000;
+
+app.use(function(req, res, next) {
+  console.log('Request of type: ' + req.method + ', to: ' + req.url);
+  next();
+});
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log('Listening on ' + port);
+});

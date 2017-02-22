6cdaa9fbda8a21e5151d3df6d252e72c79e4a4ec
Cy Kong <cykong@Regie-Kongs-MacBook-Pro.local>
2017-02-22 13:36:58 -0800

    Improve Speech to Text implementation

--- public/app.js
+++ public/app.js
@@ -1,21 +1,181 @@
- 
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var final_span = '';
+var interim_span = '';
 
- var recognition = new webkitSpeechRecognition();
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  var recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
-  recognition.interimResults = true;
-  recognition.lang = "en-US";
-  recognition.onspeechstart = function() {
-    console.log('detecting speech')
-  }
-  recognition.onresult = function(event) { 
-    var input = event.results[0][0].transcript
-    console.log('input: ', input) 
-    var formField = document.getElementById("m")
-    formField.value = input;
-  } 
+  recognition.interimResults = false;
 
-  recognition.onsoundend = function() { 
-    console.log('speech detection ended')
+  recognition.onstart = function() {
+    recognizing = true;
+    // showInfo('info_speak_now');
   };
-  recognition.start();
 
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      console.log('no speech')
+      // showInfo('info_no_speech');
+      ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+      console.log('no mic')
+      // showInfo('info_no_microphone');
+      ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+      if (event.timeStamp - start_timestamp < 100) {
+        console.log('info blocked')
+        // showInfo('info_blocked');
+      } else {
+        console.log('info denied')
+        // showInfo('info_denied');
+      }
+      ignore_onend = true;
+    }
+  };
+
+  recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
+    }
+    if (!final_transcript) {
+      // showInfo('info_start');
+      return;
+    }
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+      var range = document.createRange();
+      range.selectNode(document.getElementById('final_span'));
+      window.getSelection().addRange(range);
+    }
+    // if (create_email) {
+    //   create_email = false;
+    //   createEmail();
+    // }
+  };
+
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+    if (typeof(event.results) == 'undefined') {
+      recognition.onend = null;
+      recognition.stop();
+      upgrade();
+      return;
+    }
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    final_transcript = capitalize(final_transcript);
+    final_span.innerHTML = linebreak(final_transcript);
+    interim_span.innerHTML = linebreak(interim_transcript);
+    if (final_transcript || interim_transcript && final_transcript !== "") {
+      console.log('final_transcript: ',final_transcript)
+      $('#messages').append($('<li>').text(socket.id + ' says: ' + final_transcript));
+      final_transcript = " ";
+    }
+  };
+}
+
+function upgrade() {
+  console.log('speech to text not supported, need upgrade')
+  // showInfo('info_upgrade');
+}
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+
+function linebreak(s) {
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+}
+
+// function createEmail() {
+//   var n = final_transcript.indexOf('\n');
+//   if (n < 0 || n >= 80) {
+//     n = 40 + final_transcript.substring(40).indexOf(' ');
+//   }
+//   var subject = encodeURI(final_transcript.substring(0, n));
+//   var body = encodeURI(final_transcript.substring(n + 1));
+//   window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+// }
+
+// function copyButton() {
+//   if (recognizing) {
+//     recognizing = false;
+//     recognition.stop();
+//   }
+//   copy_button.style.display = 'none';
+//   copy_info.style.display = 'inline-block';
+//   showInfo('');
+// }
+
+// function emailButton() {
+//   if (recognizing) {
+//     create_email = true;
+//     recognizing = false;
+//     recognition.stop();
+//   } else {
+//     createEmail();
+//   }
+//   email_button.style.display = 'none';
+//   email_info.style.display = 'inline-block';
+//   showInfo('');
+// }
+
+// function startButton(event) {
+//   if (recognizing) {
+//     recognition.stop();
+//     return;
+//   }
+//   final_transcript = '';
+//   recognition.lang = select_dialect.value;
+//   recognition.start();
+//   ignore_onend = false;
+//   final_span.innerHTML = '';
+//   interim_span.innerHTML = '';
+//   start_img.src = '/intl/en/chrome/assets/common/images/content/mic-slash.gif';
+//   showInfo('info_allow');
+//   showButtons('none');
+//   start_timestamp = event.timeStamp;
+// }
+
+// function showInfo(s) {
+//   if (s) {
+//     for (var child = info.firstChild; child; child = child.nextSibling) {
+//       if (child.style) {
+//         child.style.display = child.id == s ? 'inline' : 'none';
+//       }
+//     }
+//     info.style.visibility = 'visible';
+//   } else {
+//     info.style.visibility = 'hidden';
+//   }
+// }
+
+// var current_style;
+// function showButtons(style) {
+//   if (style == current_style) {
+//     return;
+//   }
+//   current_style = style;
+//   copy_button.style.display = style;
+//   email_button.style.display = style;
+//   copy_info.style.display = 'none';
+//   email_info.style.display = 'none';
+// }
+
+recognition.start()

1bf369de402e088a8c21778dc5208d96f84594de
David Chang <dchang103@gmail.com>
2017-02-25 17:12:55 -0800

    html refactor + socket listeners, draw/text chat still broken

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+var server = require(isUseHTTPs ? 'https' : 'http');
+const http = require('http').Server(app)
+var url = require('url');
 var isUseHTTPs = false;
 var express = require('express');
 var app = express();
-
+const io = require('socket.io')(http);
 
 var port = 3000;
 
 var fs = require('fs');
 var path = require('path');
 
-if (isUseHTTPs) {
-    var options = {
-        key: fs.readFileSync('/etc/letsencrypt/live/www.will-ramsey.com/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/www.will-ramsey.com/cert.pem')
-    };
-}
+// if (isUseHTTPs) {
+//     var options = {
+//         key: fs.readFileSync('/etc/letsencrypt/live/www.will-ramsey.com/privkey.pem'),
+//         cert: fs.readFileSync('/etc/letsencrypt/live/www.will-ramsey.com/cert.pem')
+//     };
+// }
 
-var server = require(isUseHTTPs ? 'https' : 'http');
-var url = require('url');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -171,5 +172,47 @@ function runServer() {
         } catch (e) {}
     });
 }
+
+// Text chat
+//listen on the connection event for incoming sockets, and I log it to the console.
+io.on('connection', function (socket) {
+    console.log('new user ' + socket.id + ' connected');
+    //create event -- event listener on client side
+
+    io.on('disconnect', function() {
+        console.log('user ' + socket.id + ' disconnected')
+    });
+
+    io.on('chat message', function(msg){
+        io.emit('chat message:', msg);
+        //then save to the database
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('chat message sent: ', msg)
+    io.emit('chat message', msg);
+    });
+
+  socket.on('clear drawing', function(){
+    console.log('clearing drawing for everyone')
+        line_history = [];
+        io.emit('clearit', true);
+    });
+ 
+
+    //shared drawing socket - send drawing to user so they can see updated drawing
+var line_history = [];
+        
+        for(var i in line_history) {    
+            socket.emit('draw_line', {line: line_history[i]})
+        }
+
+    socket.on('draw_line', function(data) {
+
+        console.log('server is receiving data from:', data)
+        line_history.push(data.line);
+        io.emit('draw_line', {line: data.line})
+    })
+});
 
 runServer();

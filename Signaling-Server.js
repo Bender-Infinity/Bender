56c7d8e72211d9ef477b6013b8dc51730b4f033c
David Chang <dchang103@gmail.com>
2017-02-27 14:46:32 -0800

    mashed signalingserver and custom io together into abomination

--- Signaling-Server.js
+++ Signaling-Server.js
@@ -75,6 +75,38 @@ module.exports = exports = function(app, socketCallback) {
 
         socket.userid = params.userid;
         appendUser(socket);
+        //
+        io.on('disconnect', function() {
+          console.log('user ' + socket.id + ' disconnected')
+        });
+
+        io.on('chat message', function(msg){
+          io.emit('chat message:', msg);
+          //then save to the database
+        });
+
+        socket.on('chat message', function(msg){
+          console.log('chat message sent: ', msg)
+          io.emit('chat message', msg);
+        });
+
+        socket.on('clear drawing', function(){
+          console.log('clearing drawing for everyone')
+          line_history = [];
+          io.emit('clearit', true);
+        });
+        //
+      //shared drawing socket - send drawing to user so they can see updated drawing
+        var line_history = [];
+          for(var i in line_history) {  
+            socket.emit('draw_line', {line: line_history[i]})
+          }
+
+          socket.on('draw_line', function(data) {
+            console.log('server is receiving data from:', data)
+            line_history.push(data.line);
+            io.emit('draw_line', {line: data.line})
+          })
 
         socket.on('extra-data-updated', function(extra) {
             try {

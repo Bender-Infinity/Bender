336d51a3695a02f66eb70649dad43a50780462fb
William Ramsey <wramsey@gmail.com>
2017-03-06 08:42:36 -0800

    Add test scaffolding

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const config = {
- entry: './client/App.jsx',
- output: {
-   filename: 'bundle.js',
-   path: './public'
- },
- module: {
-   loaders: [
-     {
-       test: /\.jsx$/,
-       exclude: /(node_modules|bower_components)/,
-       loader: 'babel-loader',
-       query: {
-         presets: ["react", "es2015"]
-       }
-     }
-   ]
- }
+  entry: './client/App.jsx',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/public',
+    path: __dirname
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["react", "es2015"]
+        }  
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
 
 module.exports = config;

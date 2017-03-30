afed877176a1cd7bca38ca959015369c550a0405
Cy Kong <cykong@Regie-Kongs-MacBook-Pro.local>
2017-03-30 12:23:30 -0700

    pushing to instance

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const config = {
   output: {
     filename: 'bundle.js',
     publicPath: '/public',
-    path: './public'
+    path: __dirname + '/public'
   },
   module: {
     loaders: [
@@ -20,22 +20,22 @@ const config = {
       }
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  ],
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin(),
+  //   new webpack.optimize.AggressiveMergingPlugin(),
+  //   new CompressionPlugin({
+  //     asset: "[path].gz[query]",
+  //     algorithm: "gzip",
+  //     test: /\.js$|\.css$|\.html$/,
+  //     threshold: 10240,
+  //     minRatio: 0.8
+  //   })
+  // ],
   resolve: {
     extensions: ['.js', '.jsx']
   }
